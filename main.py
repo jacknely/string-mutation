b26792f2305331f69ca9308e74b1ad3f401c996e
Jack Nelson <jack.nelson@tails.com>
2022-09-03 18:48:34 +0100

    feat(A4): Add click to control application in cli

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+#!/usr/bin/python3
 """A string mutation application."""
+import click
+
 from src.string_mutate import StringMutateService
 
 
-def string_mutation():
+@click.command()
+@click.option('-w/-r')
+@click.option(
+    '--string', '-s', type=str, multiple=False, help='A string to mutate'
+)
+def string_mutation(string, w):
     """Mutate a string using the string mutate module."""
     service = StringMutateService()
-    service.add_string('test_string string test for testing')
-    reversed_string = service.reverse_words()
+    service.add_string(string)
+    if w:
+        reversed_string = service.reverse_words()
+    else:
+        reversed_string = service.reverse()
 
-    print(reversed_string)
+    click.echo(reversed_string)
 
 
 if __name__ == '__main__':
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,44 @@
 """A collection of tests for the main application."""
+from click.testing import CliRunner
+
 from main import string_mutation
 
 
-def test_main(capsys):
+def test_main():
     """Test that main returns the expected response."""
-    string_mutation()
-    captured = capsys.readouterr()
-    assert captured.out.strip() == 'testing for test string test_string'
+    runner = CliRunner()
+    result = runner.invoke(string_mutation, ['-s', 'test me please.'])
+    assert result.exit_code == 0
+    assert result.output == '.esaelp em tset\n'
+
+
+def test_main_with_r():
+    """Test that main returns the expected response."""
+    runner = CliRunner()
+    result = runner.invoke(string_mutation, ['-s', 'test me please.', '-r'])
+    assert result.exit_code == 0
+    assert result.output == '.esaelp em tset\n'
+
+
+def test_main_with_w():
+    """Test that main returns the expected response."""
+    runner = CliRunner()
+    result = runner.invoke(string_mutation, ['-s', 'test me please.', '-w'])
+    assert result.exit_code == 0
+    assert result.output == 'please. me test\n'
+
+
+def test_main_program_exits_with_no_params():
+    """Test that main exits with code 2."""
+    runner = CliRunner()
+    result = runner.invoke(string_mutation, ['-s'])
+    assert result.exit_code == 2
+    assert "Error: Option '-s' requires an argument.\n" in result.output
+
+
+def test_main_raises_exits_with_code_2():
+    """Test that main exits with code 2."""
+    runner = CliRunner()
+    result = runner.invoke(string_mutation, ['-s', 'test', 'me', '-r'])
+    assert result.exit_code == 2
+    assert 'Got unexpected extra argument (me)' in result.output
